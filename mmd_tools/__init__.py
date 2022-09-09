6930754ddf4166e23cd145365b03b86d834ca7e4
UuuNyaa <uuunyaa@gmail.com>
2022-09-09 01:10:29 -0700

    Release preparation

--- mmd_tools/__init__.py
+++ mmd_tools/__init__.py
@@ -3,8 +3,8 @@
 bl_info = {
     "name": "mmd_tools",
     "author": "sugiany",
-    "version": (2, 5, 1),
-    "blender": (2, 83, 0),
+    "version": (2, 6, 0),
+    "blender": (2, 93, 0),
     "location": "View3D > Sidebar > MMD Tools Panel",
     "description": "Utility tools for MMD model editing. (UuuNyaa's forked version)",
     "warning": "",
